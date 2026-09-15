63bbafe184448c1f75a7e65c6b10665d790c3a11
Mikheil Berishvili <mikheil.berishvili12@gmail.com>
2026-09-15 22:08:19 +0400

    railway-bar 0.1.2

--- Casks/railway-bar.rb
+++ Casks/railway-bar.rb
@@ -1,6 +1,6 @@
 cask "railway-bar" do
-  version "0.1.1"
-  sha256 "b21e30932b8f522091c7523514fe39c5c292f3c974b0aa1754a0dfd1eb467075"
+  version "0.1.2"
+  sha256 "b7b1b8e7bbd7230e99331ad0af6aea8134adc780a1b6a509fbe706504be6bd0d"
 
   url "https://github.com/mberrishdev/railwaybar-releases/releases/download/v#{version}/RailwayBar-#{version}.dmg"
   name "Railway Bar"
